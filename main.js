49f3b3f2859613f20bf7d8343086e9a87fa62555
tahaawan123 <tahaawan598@gmail.com>
2024-05-16 23:53:31 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,24 @@
+// Q2.Evaluating a number game:
+// • Prompt the user to enter a number.
+// • Compare the entered number with a dynamic number value.
+// • Output the result indicating whether the entered number is greater than, equal to, or less 
+// than the dynamic number value.
+import inquirer from "inquirer";
+let num = 6;
+let user_Ans = await inquirer.prompt([{
+        name: "nummberGame",
+        type: "number",
+        message: "Please Enter Your Number :"
+    }]);
+if (user_Ans.nummberGame === num) {
+    console.log(`Congratulation! Your Entered Number is Equal to the Dynamic Number Value `);
+}
+else if (user_Ans.nummberGame < num) {
+    console.log(`your Entered Number is less than to the Dynamic Number Value `);
+}
+else if (user_Ans.nummberGame > num) {
+    console.log(`your Entered Number is Greater than to the Dynamic Number Value`);
+}
+else {
+    console.log(`Please Enter the Number`);
+}
